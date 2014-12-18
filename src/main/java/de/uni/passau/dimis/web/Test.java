3e53a0dde6e6cd494f76cc70b2236c3c39a5f1e2
Kai Schlegel <kai.schlegel@googlemail.com>
2014-12-18 11:11:49 +0100

    initial commit

---
+++ src/main/java/de/uni/passau/dimis/web/Test.java
@@ -0,0 +1,19 @@
+package de.uni.passau.dimis.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+;
+
+@Controller
+public class Test {
+
+    @RequestMapping(value="/test", method = RequestMethod.GET)
+    @ResponseBody
+    public String test() {
+        return "Test";
+    }
+
+}
